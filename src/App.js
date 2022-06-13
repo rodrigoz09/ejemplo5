e11462db634b1761e2ec93f389551e0cc9ec6d53
rodrigoz09 <facultadrodrigozapata@gmail.com>
2022-06-13 04:30:36 -0300

    test hola mundo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo en React.
+          Hola Mundo en React version 2.
         </p>
         <a
           className="App-link"
