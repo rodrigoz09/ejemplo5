cfb051628ab5cec059b2bc4a9ba4a8670c371191
rodrigoz09 <facultadrodrigozapata@gmail.com>
2022-06-13 05:55:53 -0300

    arreglos de bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo en Angular .
+          Hola Mundo en React .
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Necesito que aparezca esta frase', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hola Mundo en angular/i);
+  const linkElement = screen.getByText(/Hola Mundo en react/i);
   expect(linkElement).toBeInTheDocument();
 });
